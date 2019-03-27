bbbc4a3e24ae66504fa314370560a6903b76a854
Azahara <azaferna@ucm.es>
2019-03-27 07:45:48 +0100

    26/3

--- src/main/java/com/geckop/spring/banckend/geckop/controllers/UsuarioRestController.java
+++ src/main/java/com/geckop/spring/banckend/geckop/controllers/UsuarioRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +30,23 @@ public class UsuarioRestController {
 	}
 	
     @GetMapping(path = {"/{id}"})
-    public Usuario findOne(@PathVariable("id") String id){
-        return usuarioService.findById(id);
+    public Boolean findOne(@PathVariable("id") String id){
+        if( usuarioService.findById(id) != null)
+        	return true;
+        else 
+        	return false;
     }	
+    
+   /* @PostMapping("/login")
+    public Boolean comprobarLogin( @RequestBody String id, @RequestBody String pass) {
+    	Usuario user = usuarioService.findById(id);
+    	if(  user != null)
+        	if(user.getPassword() == pass)
+        		return true;
+        	else
+        		return false;
+        else 
+        	return false;
+    	
+    }*/
 }
