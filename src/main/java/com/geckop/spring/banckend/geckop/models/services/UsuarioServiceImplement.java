e9a6bdfae7dd8b28e26043941fd164394f582eb3
Azahara <azaferna@ucm.es>
2019-03-28 17:44:59 +0100

    @Service en usuario

--- src/main/java/com/geckop/spring/banckend/geckop/models/services/UsuarioServiceImplement.java
+++ src/main/java/com/geckop/spring/banckend/geckop/models/services/UsuarioServiceImplement.java
@@ -3,10 +3,12 @@ package com.geckop.spring.banckend.geckop.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.geckop.spring.banckend.geckop.models.dao.IUsuarioDao;
 import com.geckop.spring.banckend.geckop.models.entity.Usuario;
 
+@Service
 public class UsuarioServiceImplement implements IUsuarioService {
 	 /*Implementamos los metodos que establezcamos en la interfaz*/
 	
